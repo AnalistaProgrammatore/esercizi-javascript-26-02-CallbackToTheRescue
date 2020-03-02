8c0051b7edeb14b77641de23a8c108e7b6b72cf9
CallbackToTheRescue <59921779+CallbackToTheRescue@users.noreply.github.com>
2020-03-03 00:32:27 +0100

    Update some.js

--- some.js
+++ some.js
@@ -1,1 +1,21 @@
+const A = [1,2,3,4]
+
+const mySomeHow = (arr,f) => {
+  let toReturn = []
+  for (let i = 0; i < arr.length; i++){
+    toReturn=f(arr[i])
+  }
+  return toReturn
+}
+const moreThanTwo = element => element > 2
+const B = mySomeHow(A,moreThanTwo)
+
+
+
+const mySomeWithReducer = (arr,f) => {
+  const toReturn = f(arr)
+  return toReturn
+}
+const moreThanTwoForReducer = arr => arr.reduce((acc, item) => (item > 2) ?  true : false, [])
+const C = mySomeWithReducer(A,moreThanTwoForReducer)
 
