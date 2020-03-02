6c0b09426ef48a11a50c0b4fd964260b00c4aa03
CallbackToTheRescue <59921779+CallbackToTheRescue@users.noreply.github.com>
2020-03-03 00:50:13 +0100

    Update generator.js

--- generator.js
+++ generator.js
@@ -1,9 +1,8 @@
-
 const generator = (...arr) =>  arr
 const A=generator(40,30,10,2,3,4,6)
 
 
-function generator2(...args) {
+const generator2 =(...args) =>{
   return Array.from(args);
 }
 const B=generator2(40,30,10,2,3,4,6)
