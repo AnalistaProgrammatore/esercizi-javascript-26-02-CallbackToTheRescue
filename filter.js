41b7ccdc6b9bf78f3f82dbacd138a66fafd78635
CallbackToTheRescue <59921779+CallbackToTheRescue@users.noreply.github.com>
2020-03-03 00:37:46 +0100

    Update filter.js

--- filter.js
+++ filter.js
@@ -1,1 +1,27 @@
 
+const A = [1,2,3,4]
+
+const myFilterHow = (arr,callback) => {
+  let toReturn = []
+  for (let i = 0; i < arr.length; i++){
+    (callback(arr[i]))?
+      toReturn = [
+        ...toReturn,
+        ...[(arr[i])]
+      ] : []
+  }
+  return toReturn
+}
+
+const moreThanTwo = elem => elem > 2
+const B = myFilterHow(A,moreThanTwo)
+
+
+
+const myFilterWithReducer = (arr,callback) => {
+  const toReturn = callback(arr)
+  return toReturn
+}
+
+const moreThanTwoForReducer = arr=> arr.reduce((cur,val)=>  val > 2 ? [...cur,val] : [])
+const C = myFilterWithReducer(A,moreThanTwoForReducer)
