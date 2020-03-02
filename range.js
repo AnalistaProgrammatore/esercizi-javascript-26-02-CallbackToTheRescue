1466a1078ef3a8e4bff56d3acb19d4a862465fb5
CallbackToTheRescue <59921779+CallbackToTheRescue@users.noreply.github.com>
2020-03-03 00:23:53 +0100

    Update range.js

--- range.js
+++ range.js
@@ -1,3 +1,11 @@
 /**
 * Scrivere qui il codice dell'esercizio 1 del capitolo 4 di eloquentJS
 **/
+const range = (start, stop, step) => Array.from({ length: (stop - start) / step }, (v, i) => start + (i * step))
+const rangeA = range(0,10,1)
+
+const range = (start= 0, stop= 10, step= 1) => Array.from({ length: (stop - start) / step }, (v, i) => start + (i * step))
+const rangeB = range()
+
+const range = (start, stop, step) => Array.from({ length: (stop - start) / step }, (v, i) => start + (i * step)).reduce((a,b) => a+b)
+const rangeC =range(0,10,1)
