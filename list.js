19891a8812bdeb071a11a072ef455046c611b3b0
CallbackToTheRescue <59921779+CallbackToTheRescue@users.noreply.github.com>
2020-03-03 00:48:26 +0100

    Update list.js

--- list.js
+++ list.js
@@ -1,3 +1,22 @@
 /**
 * Scrivere qui il codice dell'esercizio 3 del capitolo 4 di eloquentJS
 **/
+const arrayToList = arr=> {
+    let nested = null
+    for (let i = 0; i < arr.length; i++) {
+        nested = { value: arr[i], rest: nested }
+    }
+    return nested
+}
+const A =arrayToList([10, 20])
+
+const listToArray= list=> {
+  let arr = []
+  for (let i = list;i; i = i.rest) {
+    arr =[ 
+    ...arr,
+    i.value]
+  }
+  return arr;
+}
+const B = listToArray({value: 10, rest: {value: 20, rest: null}})
