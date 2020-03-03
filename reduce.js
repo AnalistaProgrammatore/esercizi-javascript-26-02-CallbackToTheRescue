209e8083b0ccb7b5347e1b03e10880f1f332ea73
CallbackToTheRescue <59921779+CallbackToTheRescue@users.noreply.github.com>
2020-03-03 01:02:57 +0100

    Update reduce.js

--- reduce.js
+++ reduce.js
@@ -1,1 +1,14 @@
-
+const A = [1,2,3,4]
+const myReduce = (arr, initialValue,reducer) => {
+  if(arr.length === 0) {
+    return initialValue;
+  }else {
+    const [first, ...rest] = arr
+    const update = reducer(initialValue,first)
+    return myReduce (rest,update,reducer)
+  }
+}
+const sum = ((a,b) => a+b)
+const even = ((a,b) => b % 2 === 0)
+const B = myReduce(A,0,sum)
+const C = myReduce(A,0,even)
